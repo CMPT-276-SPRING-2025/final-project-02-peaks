48865ce98a3a8acdf9b03188e633759f385a05e4
elenaktnguyen <elenanguyen2005@gmail.com>
2025-03-19 01:04:58 -0700

    feat: add logo bar

--- miso-hungry/src/App.jsx
+++ miso-hungry/src/App.jsx
@@ -1,11 +1,14 @@
 import Homepage from './Homepage.jsx';
 import Form from './Form.jsx';
 import Learn from './Learn.jsx';
+import Logo from './Logo.jsx';
+
 import './App.css';
 
 function App() {
   return (
     <>
+      <Logo/>
       <Homepage/>
       <Form/>
       <Learn/>
---
+++ miso-hungry/src/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <>
+        <div className="logo-container">
+            <span class="dot"></span>
+            <h1>Logo</h1>
+        </div>
+    </>
+  );
+}
+
+export default Logo;
